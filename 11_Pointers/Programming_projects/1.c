35b9b2e82f7821a710c9fcfe2d1813a1f748e787
Millan Castro <m.castrovilarino@gmail.com>
2021-12-06 06:46:01 +0100

    chapter 11: finished

---
+++ 11_Pointers/Programming_projects/1.c
@@ -0,0 +1,20 @@
+/**********************************************************
+ * Enter a dollar ammount and shows how to pay it using   *
+ * 20, 10, 5 and 1 bill                                   *
+ **********************************************************/
+
+#include <stdio.h>
+
+
+
+int main() {
+    int *p = NULL, *q, *r;
+
+    
+
+    printf("p: %p\n", p);
+    printf("q: %p\n", q);
+    printf("r: %p\n", r);
+
+    return 0;
+}
---
+++ 11_Pointers/Programming_projects/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){
+    int *p;
+
+    *p = 0;
+
+    printf("content of p is %d\n", *p);
+
+    return 0;
+}
